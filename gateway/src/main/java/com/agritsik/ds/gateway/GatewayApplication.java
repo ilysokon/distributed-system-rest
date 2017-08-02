5d411779013e4d5a5f3ca96c7ca53c9898cbdfa0
Lysokon <igor.lysokon@wirecard.lan>
2017-08-02 14:02:25 +0200

    just minor port change

--- gateway/src/main/java/com/agritsik/ds/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/agritsik/ds/gateway/GatewayApplication.java
@@ -6,10 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-
-
-
-
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
